ecbc0d90ff2ca0b28550b43c13bb87741ea65e4c
Prerna Katyal <your_github_email_here>
2026-03-02 12:02:28 +0530

    Day 1 JS practice

---
+++ first.js
@@ -0,0 +1,18 @@
+console.log("prerna");
+let age=24;
+console.log(age);
+let a;
+console.log(a);
+const student=
+{
+    a:"Perna",
+    b:24,
+
+}
+for(let count=0;count<1000;count++)
+{
+    console.log("prerna")
+}
+let sentence=`this is a template literal`;
+console.log(sentence);
+console.log(typeof sentence);
